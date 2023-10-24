54519ab3d415070e3bb5cb581ebff6798dd9694f
mbvkken <80825792+mbvkken@users.noreply.github.com>
2023-10-24 10:44:35 +0200

    added checkDataExtension.js script

---
+++ scripts/checkDataExtension.js
@@ -0,0 +1,54 @@
+const axios = require('axios');
+
+const SERVER_BASE_URL = 'http://localhost:3000'; // Adjust this to the address where your server runs
+
+async function notifySlack(message) {
+  try {
+    await axios.post(`${SERVER_BASE_URL}/api/sendToSlack`, {
+      text: message
+    });
+  } catch (error) {
+    console.error('Failed to send Slack notification:', error);
+  }
+}
+
+function isValidURL(string) {
+  try {
+    new URL(string);
+    return true;
+  } catch (_) {
+    return false;
+  }
+}
+
+async function checkDataExtension() {
+  try {
+    const response = await axios.get(`${SERVER_BASE_URL}/api/data-extension?id=YOUR_DATA_EXTENSION_ID`);
+    const fetchedData = response.data;
+
+    // Check the total number of records
+    if (fetchedData.items.length < 100) {
+      const adminPanelURL = "https://mc.s50.exacttarget.com/cloud/#app/Automation%20Studio/AutomationStudioFuel3/";
+      const message = `On latest import the Data Extension "${fetchedData.name}" has ${fetchedData.items.length} records which is less than the expected 100 records. This could be correct, but maybe worth checking out? Head over to <${adminPanelURL}|Automation Studio> `;
+      notifySlack(message);
+    }
+
+    // Check if survey_url is valid for each item
+    let invalidURLs = 0;
+    for (const item of fetchedData.items) {
+      if (!isValidURL(item.values.survey_url)) {
+        invalidURLs++;
+      }
+    }
+    if (invalidURLs > 0) {
+      const message = `${invalidURLs} or more invalid URLs detected in "${fetchedData.name}"`;
+      notifySlack(message);
+    }
+
+  } catch (err) {
+    console.error('Error occurred:', err.message);
+  }
+}
+
+// Invoke the function to check the data extension
+checkDataExtension();
