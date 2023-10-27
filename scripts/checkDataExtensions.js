a0b9d2d9e84f6d260ad71ed808384423560fd29c
mbvkken <80825792+mbvkken@users.noreply.github.com>
2023-10-27 18:03:54 +0200

    new update

--- scripts/checkDataExtensions.js
+++ scripts/checkDataExtensions.js
@@ -3,13 +3,23 @@ const axios = require('axios');
 const SERVER_BASE_URL = process.env.VERCEL_SERVER_BASE_URL || 'http://localhost:3000'; // Adjust this to the address where your server runs
 const DATA_EXTENSION_KEYS = process.env.MC_DE_KEYS ? process.env.MC_DE_KEYS.split(',') : [];
 
+const sentNotifications = new Set(); // Create a Set to store sent notifications
+
 async function notifySlack(message, dataExtensionName) {
+  const notificationKey = `${dataExtensionName}:${message}`;
+
+  if (sentNotifications.has(notificationKey)) {
+    console.log('Notification already sent:', message);
+    return;
+  }
+
   try {
     await axios.post(`${SERVER_BASE_URL}/api/sendToSlack`, {
       text: message,
       dataExtensionName: dataExtensionName, // Include dataExtensionName in the payload
     });
     console.log('Slack notification sent:', message);
+    sentNotifications.add(notificationKey); // Add the notification to the Set to mark it as sent
   } catch (error) {
     console.error('Failed to send Slack notification:', error);
   }
@@ -33,17 +43,15 @@ async function checkDataExtension(dataExtensionKey) {
     for (const dataExtension of fetchedData) {
       console.log(`Data Extension: ${dataExtension.name}`);
 
-      if (dataExtension.name === 'medallia_rnps_end_user_import_url') {
-        // Check the total number of records for 'medallia_rnps_end_user_import_url'
-        if (dataExtension.items.length < 1001) {
-          const adminPanelURL = "https://mc.s50.exacttarget.com/cloud/#app/Automation%20Studio/AutomationStudioFuel3/";
-          const vercelURL = 'https://sfmc-app-monitoring.vercel.app/';
-          const message = `Check status <${vercelURL}|here>: On the latest import, the Data Extension "${dataExtension.name}" has ${dataExtension.items.length} records which is less than the expected 100 records. This could be correct, but maybe worth checking out? Head over to <${adminPanelURL}|Automation Studio>`;
-          notifySlack(message, dataExtension.name);
-        }
+      // Check the total number of records for this data extension
+      if (dataExtension.items.length < 100 && dataExtension.name === 'medallia_rnps_end_user_import_url') {
+        const adminPanelURL = "https://mc.s50.exacttarget.com/cloud/#app/Automation%20Studio/AutomationStudioFuel3/";
+        const vercelURL = 'https://sfmc-app-monitoring.vercel.app/';
+        const message = `Check status <${vercelURL}|here>: On the latest import, the Data Extension "${dataExtension.name}" has ${dataExtension.items.length} records which is less than the expected 100 records. This could be correct, but maybe worth checking out? Head over to <${adminPanelURL}|Automation Studio>`;
+        notifySlack(message, dataExtension.name);
       }
 
-      // Check if survey_url is valid for each item in all data extensions
+      // Check if survey_url is valid for each item in this data extension
       for (const item of dataExtension.items) {
         if (!isValidURL(item.values.survey_url)) {
           const vercelURL = 'https://sfmc-app-monitoring.vercel.app/';
@@ -56,8 +64,6 @@ async function checkDataExtension(dataExtensionKey) {
     console.error(`Error occurred while processing Data Extension ${dataExtensionKey}:`, err.message);
   }
 }
-
-
 
 // Loop through the array of DATA_EXTENSION_KEYS and check each data extension
 async function checkDataExtensions(dataExtensionKeys) {
