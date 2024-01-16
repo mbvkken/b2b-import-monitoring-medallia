a6fc04fed00c7b6e7285a5a7a5033b7905d6da4a
mbvkken <80825792+mbvkken@users.noreply.github.com>
2024-01-16 15:47:30 +0100

    made changes to header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,3 @@
-// components/Header.js
-
 import Link from 'next/link';
 import { FaHome, FaSearch, FaCogs } from 'react-icons/fa'; // Assuming FaCogs is the icon for Automations
 import styles from '../styles/Header.module.css';
@@ -7,22 +5,31 @@ import styles from '../styles/Header.module.css';
 const Header = () => {
   return (
     <div className={styles.header}>
-      <div className={styles.container}>
-        <div className={styles.title}>Telia SFMC B2B</div>
-        <div className={styles.buttonContainer}>
-          <Link href="/">
-            <a className={styles.button}><FaHome /></a> {/* Updated to include 'a' tag */}
-          </Link>
-          <Link href="/search">
-            <a className={styles.button}><FaSearch /></a> {/* Updated to include 'a' tag */}
-          </Link>
-          <Link href="/automations">
-            <a className={styles.button}><FaCogs /></a> {/* New link for Automations */}
-          </Link>
-        </div>
-      </div>
+      <div className={styles.container}>    
+      <div className={styles.title}>Telia SFMC B2B</div>
+    <div className={styles.buttonContainer}>
+      <Link href="/">
+        <span className={styles.button}>
+          <FaHome />
+          <span className={styles.buttonLabel}>Data Extensions</span>
+        </span>
+      </Link>
+      <Link href="/search">
+        <span className={styles.button}>
+          <FaSearch />
+          <span className={styles.buttonLabel}>Journeys</span>
+        </span>
+      </Link>
+      <Link href="/automations">
+        <span className={styles.button}>
+          <FaCogs />
+          <span className={styles.buttonLabel}>Automations</span>
+        </span>
+      </Link>
     </div>
-  );
+  </div>
+</div>
+);
 };
 
 export default Header;
